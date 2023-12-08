5023ff262b623ed46d62f33d4d961cf7931a485d
Markus <markussmadsbakken@gmail.com>
2023-12-08 14:55:59 +0100

    denne gangen er den ferdig lover

--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -8,6 +8,6 @@ export async function GET() {
 
 export async function POST(request: Request) {
     const { author, quote, context, writtenBy } = await request.json();
-    console.log("authot" + author, "quote" + quote, "context" + context, "written" + writtenBy)
-    await sql`INSERT INTO quotes (author, quote, context, writtenBy, date) VALUES (${author}, ${quote}, ${context}, ${writtenBy}, ${new Date().getFullYear()}-${new Date().getMonth().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })}-${new Date().getDate().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })})`;
+    await sql`INSERT INTO quotes (author, quote, context, writtenBy, date) VALUES (${author}, ${quote}, ${context}, ${writtenBy},${"" + new Date().getFullYear() + "-" + new Date().getMonth().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + "-" + new Date().getDate().toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })})`;
+    return NextResponse.json({ status: "success" })
 }
