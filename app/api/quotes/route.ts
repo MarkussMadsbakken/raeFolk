3647009ce1a01cf9aa1a625c6e5b84f3b01b544b
Markus <markussmadsbakken@gmail.com>
2023-12-08 14:11:20 +0100

    finished

--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -1,13 +1,14 @@
-import { kv } from '@vercel/kv';
+import { sql } from "@vercel/postgres";
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const Quotes = await kv.hgetall("quotes")
+    const Quotes = await sql`SELECT * FROM quotes`;
     return NextResponse.json(Quotes);
 }
 
 export async function POST(request: Request) {
     const { author, quote, context, writtenBy } = await request.json();
+    console.log(author, quote, context, writtenBy)
     const data = {
         author: author,
         quote: quote,
@@ -15,7 +16,5 @@ export async function POST(request: Request) {
         writtenBy: writtenBy,
         date: new Date().toLocaleString()
     }
-
-    await kv.hset("quotes", data);
-    return NextResponse.redirect('/quotes');
+    await sql`INSERT INTO quotes (author, quote, context, writtenBy, date) VALUES (${data.author}, ${data.quote}, ${data.context}, ${data.writtenBy}, ${data.date})`;
 }
