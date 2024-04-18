c6d3f5db0af59478aa5030a99342fb24d1e56f4f
MarkusSMadsbakken <113692526+MarkussMadsbakken@users.noreply.github.com>
2024-04-18 23:38:15 +0200

    oops

--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -1,8 +1,8 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from 'next/server';
 
-export async function GET({ page }: { page: number }) {
-    const Quotes = await sql`SELECT users.username as writtenby, quotes2.date, quotes2.quote, quotes2.context, authors.username as author FROM quotes2 JOIN users ON quotes2.writtenBy = users.id JOIN users AS authors ON quotes2.author = authors.id ORDER BY quotes2.id DESC LIMIT 10 OFFSET ${page * 10}`;
+export async function GET() {
+    const Quotes = await sql`SELECT users.username as writtenby, quotes2.date, quotes2.quote, quotes2.context, authors.username as author FROM quotes2 JOIN users ON quotes2.writtenBy = users.id JOIN users AS authors ON quotes2.author = authors.id ORDER BY quotes2.id DESC LIMIT 10 OFFSET 0`;
     return NextResponse.json(Quotes);
 }
 
